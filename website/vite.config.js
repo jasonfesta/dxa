0f982a90eb7d6f5cd5f78240b9f5c2bfb0390979
Jason Festa <jasonfesta@users.noreply.github.com>
2026-09-24 04:43:02 -0400

    Add README markdown link to index

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,13 +1,34 @@
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
+import { readFileSync } from "fs";
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const guideMarkdownPath = resolve(__dirname, "../README.md");
+const guideMarkdownFileName = "design-xagents.md";
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [
+    glsl(),
+    {
+      name: "dxa-guide-markdown",
+      configureServer(server) {
+        server.middlewares.use(`/${guideMarkdownFileName}`, (_request, response) => {
+          response.setHeader("Content-Type", "text/markdown; charset=utf-8");
+          response.end(readFileSync(guideMarkdownPath, "utf8"));
+        });
+      },
+      generateBundle() {
+        this.emitFile({
+          type: "asset",
+          fileName: guideMarkdownFileName,
+          source: readFileSync(guideMarkdownPath, "utf8"),
+        });
+      },
+    },
+  ],
   base: "./",
   resolve: {
     alias: {
